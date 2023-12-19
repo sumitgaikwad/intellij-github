c04f5679bb3d14e0727b0b9860a636f3d03ecffa
sumitgaikwad <gaikwad.sumit.4s@gmail.com>
2023-12-19 16:37:56 +0530

    Initial commit

---
+++ src/main/java/com/devpacticals/intellijgithub/IntellijGithubApplication.java
@@ -0,0 +1,13 @@
+package com.devpacticals.intellijgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntellijGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntellijGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devpacticals/intellijgithub/IntellijGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devpacticals.intellijgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntellijGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
